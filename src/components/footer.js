758067b74c699c084cdbe6ad3856b16fc48788cb
Its-Tanay <tantiw1910@gmail.com>
2023-10-27 02:25:28 +0530

    Added a few transitions to the components.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-      <footer className="w-screen h-[10vh] text-center flex items-center flex-col justify-center bg-black text-white tablet:text-xl laptop:text-2xl tablet:h-[15vh] laptop:h-[20vh]">
-        <p className="text-center">Made by <a className="font-extralight" href="https://github.com/Its-Tanay">Tanay Tiwari</a></p>
+      <footer className=" transition-all w-screen h-[10vh] text-center flex items-center flex-col justify-center bg-black text-white tablet:text-xl laptop:text-2xl tablet:h-[15vh] laptop:h-[20vh]">
+        <p className="transition-all text-center">Made by <a className="transition-all font-extralight" href="https://github.com/Its-Tanay">Tanay Tiwari</a></p>
       </footer>
     );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
-        <header className="w-screen h-[10vh] text-center flex items-center flex-col justify-center bg-black text-white tablet:text-[2rem] laptop:text-[2.5rem] tablet:h-[15vh] laptop:h-[20vh]">
-            <h1 className="text-center">CV Builder</h1>
+        <header className="transition-all w-screen h-[10vh] text-center flex items-center flex-col justify-center bg-black text-white tablet:text-[2rem] laptop:text-[2.5rem] tablet:h-[15vh] laptop:h-[20vh]">
+            <h1 className="transition-all text-center">CV Builder</h1>
         </header>
     )
 }
